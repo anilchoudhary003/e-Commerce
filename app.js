8845d634a6197501e5149d44f9b1ee7dac85b983
anilchoudhary003 <anilchoudhary5906@gmail.com>
2026-01-03 18:47:58 +0530

    change

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-// let h1 = document.querySelector(" .number h1");
-// h1.innertext ="1";
+
+
+ let number = document.querySelector(".number");
+ 
+//   let num = number.innerText = "0";
+let addcart = document.querySelectorAll(".AddCart");
+
+for (let i = 0; i < addcart.length; i++) {
+    addcart[i].addEventListener("click", () => {
+        console.log("Clicked:", addcart[i].innerText);
+        number.style ="opacity: 1";
+        
+        number.innerText++;
+
+      
+    });}
+
+
+
+
+
