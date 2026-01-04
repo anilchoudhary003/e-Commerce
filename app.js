19d560cce42005558772716e681a2b3276a08ca8
anilchoudhary003 <anilchoudhary5906@gmail.com>
2026-01-04 15:44:35 +0530

    newchange

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
+let number = document.querySelector(".number");
 
-
- let number = document.querySelector(".number");
- 
+// let divv = document.querySelector(".add-cart");
+// console.log(divv);
+// add.createElement("div");
+//   add.appendChild(div);
 //   let num = number.innerText = "0";
 let addcart = document.querySelectorAll(".AddCart");
 
 for (let i = 0; i < addcart.length; i++) {
     addcart[i].addEventListener("click", () => {
         console.log("Clicked:", addcart[i].innerText);
-        number.style ="opacity: 1";
-        
+        number.style = "opacity: 1";
         number.innerText++;
 
-      
-    });}
+
+    });
+}
+
+// for (let i = 0; i < addcart.length; i++) {
+//     addcart[i].addEventListener("click", () => {
+//         if (document.body.classList.contains("add-cart")) {
+
+//             const divv = document.createElement("div");
+//             divv.classList.add("Box");
+//             document.body.appendChild(divv);
+//         }
+
+//     });
+// }
 
 
+for (let i = 0; i < addcart.length; i++) {
+    addcart[i].addEventListener("click", () => {
+        console.log("create a div");
+        if (document.body.classList.contains("add-cart")) {
+
+            const divv = document.createElement("div");
+            divv.classList.add("Box");
+            document.body.appendChild(divv);
+        }
+
+    });
+}
+
+//  if (document.body.classList.contains("add-cart")) {
+
+//             const divv = document.createElement("div");
+//             divv.classList.add("Box");
+//             document.body.appendChild(divv);
+//         }
 
 
 
