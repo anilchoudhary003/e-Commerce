5bcdaf0cc9841e555367d25219bce8478bca022c
anilchoudhary003 <anilchoudhary5906@gmail.com>
2026-01-11 12:59:23 +0530

    Add Log-in pages

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ for (let i = 0; i < addcart.length; i++) {
         number.style = "opacity: 1";
         number.innerText++;
 
+        addcart[i].classList.add("on_click");
+        setTimeout(() => {
+            console.log("hii");
+            addcart[i].classList.remove("on_click");
+        }, 200)
+
+
 
     });
 }
@@ -52,3 +59,50 @@ for (let i = 0; i < addcart.length; i++) {
 
 
 
+
+
+
+
+
+
+
+
+let login = document.querySelector(".login");
+let log1 = document.querySelector(".log1");
+let log2 = document.querySelector(".log2");
+let a1 = document.querySelector(".Register1 a");
+let a2 = document.querySelector(".Register2 a");
+let main = document.querySelector(".main");
+let container = document.querySelector(".container");
+let footer = document.querySelector("footer");
+let submit = document.querySelector(".submit");
+
+
+login.addEventListener(("click"), () => {
+
+    log1.classList.remove("hidden");
+    main.classList.add("hidden");
+    container.classList.add("hidden");
+    footer.classList.add("hidden");
+
+})
+a1.addEventListener(("click"), () => {
+
+    log2.classList.remove("hidden");
+    log1.classList.add("hidden");
+})
+
+a2.addEventListener(("click"), () => {
+
+    log1.classList.remove("hidden");
+    log2.classList.add("hidden");
+})
+
+submit.addEventListener(("click"),()=>{
+    console.log("submit");
+    main.classList.remove("hidden");
+    container.classList.remove("hidden");
+    footer.classList.remove("hidden");
+     log1.classList.add("hidden");
+
+})
